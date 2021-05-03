c1dbdc4e3876e22d838d2ca77cc2fd23be25f56c
AHMED <eng.computer1@yahoo.com>
2021-05-02 20:06:18 -0500

    added styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,13 @@ class ApplicationController < ActionController::Base
 
    def create
     
-    @hash=Hashcalc.create(name:params[:name])
-    @hashed=@hash.sha2(params[:name])
-    @md5=@hash.md5(params[:name])
-    @rmd160=@hash.rmd160(params[:name])
-    @sha1=@hash.sha1(params[:name])
-    @sha3=@hash.sha3(params[:name])
-   # byebug
-    render '/application/hello'
+        @hash=Hashcalc.create(name:params[:name])
+        @hashed=@hash.sha2(params[:name])
+        @md5=@hash.md5(params[:name])
+        @rmd160=@hash.rmd160(params[:name])
+        @sha1=@hash.sha1(params[:name])
+        @sha3=@hash.sha3(params[:name])
+        render '/application/hello'
    end
 
 end
